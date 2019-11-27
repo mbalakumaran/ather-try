350d5ddffc7a73ee185d67c24410fb999da3b8a2
sketchbrahma <balakumaran@sketchbrahma.com>
2019-11-27 12:29:17 +0530

    animation

--- assets/js/charging.js
+++ assets/js/charging.js
@@ -1,30 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+	$(window).scroll(function() {
+
 	var container = $("#section-wipes"),
-		scrollPosi = $(this).scrollTop(),
-		firstSection = $("#section-one"),
-		lastSection = $("#section-three");
+	scrollPosi = $(this).scrollTop(),
+	firstSection = $("#section-one"),
+	lastSection = $("#section-three");
 
-	if (
-		scrollPosi > firstSection.offset().top &&
-		container.hasClass("scroll-start")
-	) {
+	console.log("top",firstSection.offset().top);
+	console.log("top",scrollPosi);
+	console.log("top",lastSection.offset());
+	console.log("top",container.hasClass("scroll-start"));
+
+	if (scrollPosi > firstSection.offset().top && container.hasClass("scroll-start")) {
 		container.removeClass("scroll-start");
-	} else if (
-		scrollPosi < firstSection.offset().top &&
-		!container.hasClass("scroll-start")
-	) {
+		console.log("wanted false:",container.hasClass("scroll-start"))
+	} else if ( scrollPosi < firstSection.offset().top && !container.hasClass("scroll-start")) {
 		container.addClass("scroll-start");
+		console.log("wanted true:",container.hasClass("scroll-start"))
+	}
+	
+	if (scrollPosi > lastSection.offset().top && !container.hasClass("scroll-end")) {
+		container.addClass("scroll-end");
+		console.log("wanted true:",container.hasClass("scroll-end"))
+
+	} else if ( scrollPosi < lastSection.offset().top && container.hasClass("scroll-end")) {
+		container.removeClass("scroll-end");
+		console.log("wanted false:",container.hasClass("scroll-end"))
+
 	}
 
-	if (
-		scrollPosi > lastSection.offset().top &&
-		!container.hasClass("scroll-end")
-	) {
-		container.addClass("scroll-end");
-	} else if (
-		scrollPosi < lastSection.offset().top &&
-		container.hasClass("scroll-end")
-	) {
-		container.removeClass("scroll-end");
-	}
+	});
+
 });
+
